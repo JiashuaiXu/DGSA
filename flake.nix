4de0b648b799e4f5e845307d8897b249c0698ae9
JiashuaiXu <JiashuaiXu@outlook.com>
2025-03-19 10:24:45 +0800

    mannual package uv

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "DGSA project dev shell with Python 3.8 and PyTorch 1.3";
+  description = "DGSA project dev shell with Python 3.8 and uv";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
@@ -11,17 +11,35 @@
       let
         pkgs = import nixpkgs { inherit system; };
         python = pkgs.python38;
+
+        uv = pkgs.stdenv.mkDerivation {
+          pname = "uv";
+          version = "0.1.28"; # 这里用 uv 的稳定版本
+
+          src = pkgs.fetchurl {
+            url = "https://github.com/astral-sh/uv/releases/download/0.1.28/uv-x86_64-unknown-linux-gnu.tar.gz";
+            sha256 = "sha256-91uhthg6r44v8ap5pd6jqz66zx55idp51l2v40fdhplpkzqk65yi";
+          };
+
+          phases = [ "installPhase" ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            tar -xzf $src -C $out/bin
+            chmod +x $out/bin/uv
+          '';
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             python
-            pkgs.uv
+            uv
           ];
 
           shellHook = ''
             export PYTHONNOUSERSITE="true"
             export VIRTUAL_ENV=.venv
-            echo "🟢 Python 3.8 + uv dev shell ready"
+            echo "✅ Python 3.8 + uv dev shell ready"
 
             alias python=python3.8
           '';
